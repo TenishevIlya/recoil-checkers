c0a289c0bc9934b83bf9a32ca3a3bdb1ad43edd5
Ilya Tenishev <ilya.tenishev@kodep.ru>
2023-03-19 01:10:52 +0300

    Added base layout Add css modules support Change folder structure

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+import Layout from "./scenes/Layout/Layout";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Layout />
   </React.StrictMode>
 );
